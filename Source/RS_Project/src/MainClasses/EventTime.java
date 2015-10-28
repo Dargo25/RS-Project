98bef02680ae28b1320f1e4fb82d628689867d65
Yegor <inferno2596@gmail.com>
2015-10-28 22:18:21 +0300

    Добавлены файлы классов

---
+++ Source/RS_Project/src/MainClasses/EventTime.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MainClasses;
+
+import java.sql.Time;
+import java.util.Date;
+
+/**
+ *
+ * @author Егор
+ */
+public class EventTime {
+    private Date date;
+    private Time singleEventTime;
+    private Time startTime;
+    private Time endTime;
+    private Enum type;
+}
