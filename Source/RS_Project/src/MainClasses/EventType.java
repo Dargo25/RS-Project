af05f85099ab102dc9bd030ab2ce766f38197511
Yegor <inferno2596@gmail.com>
2015-10-29 17:15:08 +0300

    Create enum

--- Source/RS_Project/src/MainClasses/EventType.java
+++ Source/RS_Project/src/MainClasses/EventType.java
@@ -5,17 +5,11 @@
  */
 package MainClasses;
 
-import java.sql.Time;
-import java.util.Date;
-
 /**
  *
  * @author Егор
  */
-public class EventTime {
-    private Date date;
-    private Time singleEventTime;
-    private Time startTime;
-    private Time endTime;
-    private EventType type;
+public enum EventType {
+    SINGLE,
+    PERIODIC
 }
