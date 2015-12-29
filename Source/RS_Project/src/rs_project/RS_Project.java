2c05bbffcbd9d701754ebb423e3351c0b5bace9c
Artem <pizzaeueu@gmail.com>
2015-12-29 18:03:34 +0300

    first refactor

--- Source/RS_Project/src/rs_project/RS_Project.java
+++ Source/RS_Project/src/rs_project/RS_Project.java
@@ -10,40 +10,18 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import MainClasses.*;
-import com.sun.deploy.trace.Trace;
-import com.sun.javafx.geom.Vec2f;
-import com.sun.javaws.Main;
-import java.awt.Image;
-import java.awt.Insets;
-import java.awt.event.MouseEvent;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.animation.ScaleTransition;
 import javafx.event.Event;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.util.Duration;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
-import org.xml.sax.SAXException;
 
 /**
  *
@@ -51,56 +29,33 @@ import org.xml.sax.SAXException;
  */
     public class RS_Project extends Application {
     private ArrayList<Subject> subjectList = new ArrayList<>();
-    Timer timer;
-    static double firstSubjectPosition = 100;
-    //ImageView imgNext;
     GridPane grid;
-    DaysOfWeek CurrentDay = DaysOfWeek.MONDAY;
-    ArrayList<Subject> CurrentSbj;
+    DaysOfWeek currentDay = DaysOfWeek.MONDAY;
+    ArrayList<Subject> currentSbj;
     ArrayList<EventImage> imgEvent;
-    
-    //public static boolean isClosed;
+
     
     frmAddSubjectEvent fm = new frmAddSubjectEvent();
-    Stage pm;
+    Stage currentStage;
+    
+    Button btnPrevious;
+    Button btnNext;
     
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("RS Project");
-        EventTimer.SetParent(this);
-        pm = primaryStage;
+        currentStage = primaryStage;
         subjectList.clear();
         ParseXML.scanXml(subjectList);
-        CurrentSbj = LoadSubjects(subjectList, CurrentDay);
-        ArrayList<Label> sbjName = WriteSubjectsToLabels(CurrentSbj);
-        imgEvent = AddEventsSumbol(CurrentSbj);
+        currentSbj = LoadSubjects(subjectList, currentDay);
+        ArrayList<Label> sbjName = WriteSubjectsToLabels(currentSbj);
+        
+        imgEvent = AddEventsSumbol(currentSbj);
                 
-        fm.SetParent(this);
-
-        
-        //Pane root = new Pane();
-
-
-        //-----------------***Инициализация компонентов формы***----------------
-        //root.getChildren().add(btn);
-        //root.getChildren().addAll(sbjName);
-        
-        Button btnNext = new Button();
-        btnNext.setText("Next Day");
-        
-        Button btnPrevious = new Button();
-        btnPrevious.setText("Previous Day");
-        
-        Label lblToday = new Label(CurrentDay.toString());
-        
-        
-        //ImageView imgEvent;
-
+        GetParent();
 
         grid = CreateGrid(sbjName);
-        grid.add(lblToday,3,0);
-        grid.add(btnNext,6, 7);
-        grid.add(btnPrevious, 2, 7);
+
         Scene scene = new Scene(grid, 300, 250);
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -111,7 +66,7 @@ import org.xml.sax.SAXException;
             @Override
             public void handle(ActionEvent event) {
                 grid.getChildren().clear();
-                CurrentDay = GetNextDay(CurrentDay);
+                currentDay = GetNextDay(currentDay);
                 start(primaryStage);
             }
         });
@@ -122,7 +77,7 @@ import org.xml.sax.SAXException;
             public void handle(ActionEvent event) {
                 grid.getChildren().clear();
                 
-                CurrentDay = GetPreviousDay(CurrentDay);
+                currentDay = GetPreviousDay(currentDay);
                 start(primaryStage);
               
             }
@@ -137,41 +92,41 @@ import org.xml.sax.SAXException;
     public static void main(String[] args) {
         launch(args);
     }
+    
+    private void GetParent(){
+        fm.SetParent(this);
+        EventTimer.SetParent(this);
+    }
 
-    private static ArrayList<Subject> LoadSubjects(ArrayList<Subject> Subjects, DaysOfWeek day) {
-       ArrayList<Subject> CurrentSubjects = new ArrayList<Subject>();
-       for (Subject sbj : Subjects) {
+    private static ArrayList<Subject> LoadSubjects(ArrayList<Subject> subjects, DaysOfWeek day) {
+       ArrayList<Subject> CurrentSubjects = new ArrayList<>();
+       for (Subject sbj : subjects) {
            if (sbj.getDay()== day){
                CurrentSubjects.add(sbj);
            }
         }
        return CurrentSubjects;
     }
-    private  ArrayList<Label> WriteSubjectsToLabels (ArrayList<Subject> Subjects){
+    private  ArrayList<Label> WriteSubjectsToLabels (ArrayList<Subject> subjects){
         ArrayList <Label> labelList = new ArrayList<>();
-        //double counter = firstSubjectPosition;
-        for (int i = 0;i<Subjects.size();i++){
-            
-            Label tempLabel = new Label(this.GetLabelName(Subjects.get(i)));
-            //tempLabel.setLayoutX(counter);
-            //tempLabel.setLayoutY(200);
+        for (Subject Subject : subjects) {
+            Label tempLabel = new Label(this.GetLabelName(Subject));
             tempLabel.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
             labelList.add(tempLabel);
             addTranslateListener(tempLabel);
-            //counter++;
         }
         return labelList;
     }
     
     public  void Refresh()
     {
-        start(this.pm);
+        start(this.currentStage);
     }
     
-    private ArrayList<EventImage> AddEventsSumbol(ArrayList<Subject> Subjects){
+    private ArrayList<EventImage> AddEventsSumbol(ArrayList<Subject> subjects){
         ArrayList<EventImage> imgList = new ArrayList<>();
-        for(int i = 0; i<Subjects.size();i++){
-            if (!Subjects.get(i).getEventList().isEmpty()){
+        for(int i = 0; i<subjects.size();i++){
+            if (!subjects.get(i).getEventList().isEmpty()){
                 ImageView imgEvent = new ImageView("images/event.png");
                 imgEvent.setFitHeight(20);
                 imgEvent.setFitWidth(20);
@@ -179,7 +134,7 @@ import org.xml.sax.SAXException;
                 EventImage userIME=new EventImage();
                 userIME.SetImg(imgEvent);
                 userIME.SetNumber(i);
-                userIME.SetEvents((ArrayList<SubjectEvent>) Subjects.get(i).getEventList());
+                userIME.SetEvents((ArrayList<SubjectEvent>) subjects.get(i).getEventList());
                 
                 imgList.add(userIME);
             }
@@ -204,23 +159,33 @@ import org.xml.sax.SAXException;
         grid.setHgap(10);
         grid.setVgap(10);
         for (int i = 0; i<sbjName.size();i++){
-        grid.add(sbjName.get(i), 3, i+1);
+            grid.add(sbjName.get(i), 3, i+1);
 
-        if (count<imgEvent.size())
-        {
-        if (imgEvent.get(count).GetNumber()==i){
-            grid.add(imgEvent.get(count).GetImg(), 4,i+1);
-            addImageClick(imgEvent.get(i));
-            count++;
-        }
-        }
+            if (count<imgEvent.size())
+            {
+                if (imgEvent.get(count).GetNumber()==i){
+                    grid.add(imgEvent.get(count).GetImg(), 4,i+1);
+                    addImageClick(imgEvent.get(i));
+                    count++;
+                }
+            }
         
-        }
+            }
+        
+        btnNext = new Button();
+        btnNext.setText("Next Day");
+        
+        btnPrevious = new Button();
+        btnPrevious.setText("Previous Day");
+        
+        Label lblToday = new Label(currentDay.toString());
+        
+        grid.add(lblToday,3,0);
+        grid.add(btnNext,6, 7);
+        grid.add(btnPrevious, 2, 7);
+        
         return grid;
     }
-    
-
-            
     
     private DaysOfWeek GetNextDay(DaysOfWeek cDay){
         if (cDay==DaysOfWeek.MONDAY){
@@ -283,7 +248,7 @@ import org.xml.sax.SAXException;
     }
       
         private Subject GetSubjectByNumber(int number){
-            for (Subject sbj: CurrentSbj){
+            for (Subject sbj: currentSbj){
                 if (sbj.getNumberInTable()==number){
                     return sbj;
                 }
@@ -293,79 +258,57 @@ import org.xml.sax.SAXException;
         }
         
         private int ParseLabel(Label lbl){
-        String sbjInfo = lbl.getText();
-        String substring = sbjInfo.substring(0, 1);
-        int number = Integer.parseInt(substring);
-        return number;
+            String sbjInfo = lbl.getText();
+            String substring = sbjInfo.substring(0, 1);
+            int number = Integer.parseInt(substring);
+            return number;
         }
-        
-        
-        
-        
-    //Что-то странное    
 
-   private void addTranslateListener(final Label node) { 
-   node.setOnMousePressed(new EventHandler() { 
+        private void addTranslateListener(final Label node) { 
+            node.setOnMousePressed(new EventHandler() { 
+            @Override
+                public void handle(Event event) {
+                    int number = ParseLabel(node);
+                    fm.SetSubject(GetSubjectByNumber(number));
+                    try {
+                        fm.start(new Stage());
+                    } 
+                    catch (Exception ex) {
+                        Logger.getLogger(RS_Project.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }    
 
-
-       @Override
-       public void handle(Event event) {
-           //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//        String sbjInfo = node.getText();
-//        String substring = sbjInfo.substring(0, 1);
-//        int number = Integer.parseInt(substring);
-        int number = ParseLabel(node);
-        //frmAddSubjectEvent fm = new frmAddSubjectEvent();
-        fm.SetSubject(GetSubjectByNumber(number));
-        //fm.SetParent(this);
-        //fm.SetSubject(null);
-       try {
-           fm.start(new Stage());
-       } catch (Exception ex) {
-           Logger.getLogger(RS_Project.class.getName()).log(Level.SEVERE, null, ex);
-       }
-       }
-       
-       
-    });
+            });
+            node.setOnMouseMoved(new EventHandler(){
+            @Override
+                public void handle(Event event) {
+                node.setFont(Font.font("Tahoma", FontWeight.NORMAL, 14));
+                }
+            });
    
-   node.setOnMouseMoved(new EventHandler(){
-   @Override
-       public void handle(Event event) {
-       node.setFont(Font.font("Tahoma", FontWeight.NORMAL, 14));
-   }
-       
-   });
+            node.setOnMouseExited(new EventHandler(){
+            @Override
+                public void handle(Event event) {
+                    node.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+                }   
+            });
+        }
    
-     node.setOnMouseExited(new EventHandler(){
-   @Override
-       public void handle(Event event) {
-       node.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
-   }
-       
-   });
-   
-   }
-   
-      private void addImageClick(final EventImage node) { 
-   node.GetImg().setOnMousePressed(new EventHandler() { 
-
-
-       @Override
-       public void handle(Event event) {
-           frmEventInfo fm = new frmEventInfo();
-           fm.SetEvents((ArrayList<SubjectEvent>) node.GetEvents());
-           try {
-               fm.start(new Stage());
-           } catch (Exception ex) {
-               Logger.getLogger(RS_Project.class.getName()).log(Level.SEVERE, null, ex);
-           }
-
-       }
-       
-       
-    });
-   }
+    private void addImageClick(final EventImage node) { 
+        node.GetImg().setOnMousePressed(new EventHandler() { 
+        @Override
+            public void handle(Event event) {
+            frmEventInfo fm = new frmEventInfo();
+            fm.SetEvents((ArrayList<SubjectEvent>) node.GetEvents());
+                try {
+                    fm.start(new Stage());
+                } 
+                catch (Exception ex) {
+                    Logger.getLogger(RS_Project.class.getName()).log(Level.SEVERE, null, ex);
+                }    
+            }    
+        });
+    }
    
 }
     
--- Source/RS_Project/src/rs_project/frmAddSubjectEvent.java
+++ Source/RS_Project/src/rs_project/frmAddSubjectEvent.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package rs_project;
 
 import javafx.application.Application;
@@ -11,51 +10,36 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import MainClasses.*;
-import java.awt.Image;
-import java.awt.Insets;
 import java.io.IOException;
 import java.text.ParseException;
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
-import javafx.event.Event;
 import javafx.geometry.Pos;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.WindowEvent;
-import javax.swing.ImageIcon;
-import javax.swing.JFormattedTextField;
-import javax.swing.JLabel;
-import javax.swing.text.MaskFormatter;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import org.xml.sax.SAXException;
-
 
 /**
  *
  * @author Samosad
  */
-public class frmAddSubjectEvent extends Application{
+public class frmAddSubjectEvent extends Application {
+
     Button btnAddEvent;
     SubjectEvent userEvent = new SubjectEvent();
     TextField txtEventName;
@@ -64,175 +48,172 @@ public class frmAddSubjectEvent extends Application{
     Subject userSubject;
     TextField txtHour;
     TextField txtMinutes;
-    
+
     static Stage ps;
     RS_Project rsProj;
-    
-    boolean isStart = true;
+
     Label lbl;
-    
-    
+
     SubjectEvent testEv;
-    
-    
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         ps = primaryStage;
-        
+
         primaryStage.setTitle("Добавить событие");
         GridPane root = CreateGrid();
-        
-        Scene scn = new Scene(root,350,200);
+
+        Scene scn = new Scene(root, 350, 200);
         primaryStage.setScene(scn);
         primaryStage.show();
-        
+
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
 
             @Override
             public void handle(WindowEvent event) {
-                isStart=true;
                 rsProj.Refresh();
             }
         });
-        
+
         btnAddEvent.setOnAction(new EventHandler<ActionEvent>() {
 
-        @Override
-        public void handle(ActionEvent event) {
-         userEvent = new SubjectEvent();
-         
-        LocalDate localDate = dpEventTime.getValue();
-        
-        userEvent = CreateEvent(localDate);
+            @Override
+            public void handle(ActionEvent event) {
+                if (Validation()) {
+                    userEvent = new SubjectEvent();
 
-        userSubject.getEventList().add(userEvent);
-        
-        WriteEventToXML(userSubject);
-        
-        
-        EventTimer.CreateTimer(userSubject, userEvent);
-        
-        Refresh();
-  
-    }
+                    LocalDate localDate = dpEventTime.getValue();
+
+                    userEvent = CreateEvent(localDate);
+
+                    userSubject.getEventList().add(userEvent);
+
+                    WriteEventToXML(userSubject);
+
+                    EventTimer.CreateTimer(userSubject, userEvent);
+
+                    Refresh();
+
+                    lbl.setText("Событие добавлено");
+                } else {
+                    lbl.setText("Заполните поля");
+
+                }
+
+            }
 
         });
     }
-    
-        private void Refresh(){
-                    try {
-                start(ps);   
-            } catch (Exception ex) {
-                Logger.getLogger(frmAddSubjectEvent.class.getName()).log(Level.SEVERE, null, ex);
-            }
-         
-            }
-    
-    private void WriteEventToXML(Subject userSubject){
-                    try {
-                ParseXML.AddNewEventToXML(userSubject);
-                //Date date = Date.from(instant);
-            } catch (SAXException ex) {
-                Logger.getLogger(frmAddSubjectEvent.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (IOException ex) {
-                Logger.getLogger(frmAddSubjectEvent.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (ParserConfigurationException ex) {
-                Logger.getLogger(frmAddSubjectEvent.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (TransformerException ex) {
-                Logger.getLogger(frmAddSubjectEvent.class.getName()).log(Level.SEVERE, null, ex);
-            }
+
+    private void Refresh() {
+        try {
+            start(ps);
+        } catch (Exception ex) {
+            Logger.getLogger(frmAddSubjectEvent.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
+
+    private void WriteEventToXML(Subject userSubject) {
+        try {
+            ParseXML.AddNewEventToXML(userSubject);
+        } catch (SAXException ex) {
+            Logger.getLogger(frmAddSubjectEvent.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(frmAddSubjectEvent.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(frmAddSubjectEvent.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TransformerException ex) {
+            Logger.getLogger(frmAddSubjectEvent.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     private void CreateTimer(SubjectEvent event) {
         Timer timer = new Timer();
 
         timer.schedule(new TimerTask() {
-            
+
             @Override
             public void run() {
                 Platform.runLater(() -> {
-                WatchEvent(event);
+                    WatchEvent(event);
                 });
             }
         }, event.getTime().getDate());
     }
-    
-    private GridPane CreateGrid() throws ParseException{
+
+    private GridPane CreateGrid() throws ParseException {
         GridPane grid = new GridPane();
-        
-        if (isStart){
         lbl = new Label();
-        }
-        else{
-            lbl = new Label("Событие было добавлено");
-        }
-             
+
         Label lblAddEvent = new Label("Введите информацию о событии");
         lblAddEvent.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        
+
         Label lblEventName = new Label("Введите заголовок события");
         txtEventName = new TextField();
-        
+
         Label lblEventContent = new Label("Введите описание события");
         txtEventContent = new TextField();
-        
+
         dpEventTime = new DatePicker();
         Label lblEventTime = new Label("Выберете дату события");
-        
+
         txtHour = new TextField();
         Label lblHours = new Label("Часы");
-        
+
         txtMinutes = new TextField();
         Label lblMinutes = new Label("Минуты");
-        
+
         grid.add(lblAddEvent, 0, 0, 2, 1);
-        
-        grid.add(lblEventName,0,1);
-        grid.add(txtEventName,1,1);
-        
-        grid.add(lblEventContent, 0,2);
-        grid.add(txtEventContent, 1,2);
-        
+
+        grid.add(lblEventName, 0, 1);
+        grid.add(txtEventName, 1, 1);
+
+        grid.add(lblEventContent, 0, 2);
+        grid.add(txtEventContent, 1, 2);
+
         grid.add(dpEventTime, 1, 3);
         grid.add(lblEventTime, 0, 3);
-        
-        grid.add(lblHours,0,4);
-        grid.add(txtHour,1,4);
-        
+
+        grid.add(lblHours, 0, 4);
+        grid.add(txtHour, 1, 4);
+
         grid.add(lblMinutes, 0, 5);
         grid.add(txtMinutes, 1, 5);
-        
-        grid.add(lbl,0,7);
-        
+
+        grid.add(lbl, 0, 7);
+
         btnAddEvent = new Button("Добавить");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(btnAddEvent);
         grid.add(hbBtn, 1, 7);
-        
+
         return grid;
     }
-    public void SetSubject(Subject sbj){
+
+    public void SetSubject(Subject sbj) {
         userSubject = sbj;
     }
-    private void WatchEvent(SubjectEvent event){
-                frmEventMessage fm = new frmEventMessage();
-                fm.SetEventName(event.getHeader());
-                fm.SetEventContent(event.getContent());
-                try {
-                    fm.start(new Stage());
-                } catch (Exception ex) {
-                    Logger.getLogger(frmAddSubjectEvent.class.getName()).log(Level.SEVERE, null, ex);
-                }
+
+    private void WatchEvent(SubjectEvent event) {
+        frmEventMessage fm = new frmEventMessage();
+        fm.SetEventName(event.getHeader());
+        fm.SetEventContent(event.getContent());
+        try {
+            fm.start(new Stage());
+        } catch (Exception ex) {
+            Logger.getLogger(frmAddSubjectEvent.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    public void SetParent (RS_Project prj){
+
+    public void SetParent(RS_Project prj) {
         rsProj = prj;
-    } 
-    
-    private SubjectEvent CreateEvent(LocalDate localDate){
-         
+    }
+
+    private SubjectEvent CreateEvent(LocalDate localDate) {
+
         SubjectEvent userEv = new SubjectEvent();
 
         int year = localDate.getYear();
@@ -241,14 +222,20 @@ public class frmAddSubjectEvent extends Application{
         int hour = Integer.parseInt(txtHour.getText());
         int minutes = Integer.parseInt(txtMinutes.getText());
         EventTime userTime = new EventTime();
-        userTime.setDate(new Date(year-1900, month-1, day, hour, minutes));
+        userTime.setDate(new Date(year - 1900, month - 1, day, hour, minutes));
 
         userEv.setTime(userTime);
-        
+
         userEv.setHeader(txtEventName.getText());
         userEv.setContent(txtEventContent.getText());
         userEv.setType(EventType.SINGLE);
-        
+
         return userEv;
+    }
+
+    private boolean Validation() {
+        return !(txtEventName.getText().isEmpty() || txtEventContent.getText().isEmpty()
+                || txtHour.getText().isEmpty() || txtMinutes.getText().isEmpty()
+                || dpEventTime.getValue() == null);
     }
 }
